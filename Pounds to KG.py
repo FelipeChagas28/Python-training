817e73ffe738e7b930be07ddf7f06ee62652be2c
Felipe Chagas <FelipeSChagas28@gmail.com>
2024-03-26 11:40:40 -0300

    Add files via upload

---
+++ Pounds to KG.py
@@ -0,0 +1,11 @@
+print("Hi! this is a program to convert pounds into kg!")
+
+name = input("What's your name?: ")
+pounds = input("How many pounds do you have?: ")
+
+# 1 pound = 0.453 kg
+
+kg = 0.453 * float(pounds)
+
+print("You have "+ str(kg) + " KG")
+
---
+++ Variables.py
@@ -0,0 +1,14 @@
+birth = input("What's your birth year?: ")
+
+#Type is to show the class of the variable. Like string, bool or int!
+print(type(birth))
+
+#Variable conversion to string to int to do calculation
+age = 2024 - int(birth)
+
+print(type(age))
+print("You have: " + str(age))
+
+
+
+
