4324f043a5c885f103661db9487aa3e08b10e4f7
Felipe Chagas <FelipeSChagas28@gmail.com>
2024-03-27 23:56:36 -0300

    Add files via upload

---
+++ formated strings.py
@@ -0,0 +1,15 @@
+first = "Felipe"
+last = "Chagas"
+
+test = f"{first} {last} is a coder"
+
+print(test)
+
+print("---------------------------------")
+
+#you can use this function to change the type of a print
+print(first.find("F"))
+print(first.upper())
+
+
+
---
+++ string test.py
@@ -0,0 +1,18 @@
+test = "Hello"
+
+#it will copy the things into the other variable
+test2 = test[:]
+
+#this will show the letter that you put into brackets, it starts in 0
+print(test[1])
+
+#the same function but in contrary
+print(test[-1])
+
+#it will show the first 2 letters
+print(test[0:2])
+
+#it won't show the first and the last caracter
+print(test[1:-1])
+
+print(test2[:])
